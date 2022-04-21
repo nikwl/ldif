8387f0ae50565b755de70e9d1f77bef43803ac80
nikwl <nikolas.lamb@gmail.com>
2022-04-21 17:56:53 +0000

    run

--- run.sh
+++ run.sh
@@ -3,3 +3,9 @@ python meshes2dataset.py \
     --dataset_directory /media/DATACENTER2/nikolas/dev/data/mendnet/datasets/ldif/v3.split_jars_spline_dataset \
     --optimize \
     --optimize_only
+
+python train.py \
+    --dataset_directory /media/DATACENTER2/nikolas/dev/data/mendnet/datasets/ldif/v3.split_jars_spline_dataset \
+    --experiment_name experiments/test_bs20 \
+    --model_type ldif \
+    --batch_size 20
