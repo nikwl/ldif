6bb6e7d6ad421d2e33f430b38550a9b93841953f
nikwl <nikolas.lamb@gmail.com>
2022-04-21 17:08:58 +0000

    added run.sh

---
+++ run.sh
@@ -0,0 +1,5 @@
+python meshes2dataset.py \
+    --mesh_directory /media/DATACENTER2/nikolas/dev/data/mendnet/datasets/ldif/v3.split_jars_spline \
+    --dataset_directory /media/DATACENTER2/nikolas/dev/data/mendnet/datasets/ldif/v3.split_jars_spline_dataset \
+    --optimize \
+    --optimize_only
